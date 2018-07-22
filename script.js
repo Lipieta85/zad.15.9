1169c2baab0193499db92e672d93b6fe179cbe3b
Rafał Lipieta <cwgflojdid@wp.pl>
2018-07-22 19:11:36 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchText: '',
+            users: []
+        };
+    }
+
+    onChangeHandle(event) {
+        this.setState({ searchText: event.target.value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        const { searchText } = this.state;
+        const url = `https://api.github.com/search/users?q=${searchText}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(responseJson => this.setState({ users: responseJson.items }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>GITHUB USERS SEARCH</h1>
+                <form onSubmit={event => this.onSubmit(event)} className={'form-group'}> 
+                    <label htmlFor="searchText">Search by user name</label>
+                    <input
+                        type="text"
+                        id="searchText"
+                        onChange={event => this.onChangeHandle(event)}
+                        value={this.state.searchText} />
+                </form>
+                <UsersList users={this.state.users} />
+            </div>
+        );
+    }
+}
+
+class UsersList extends React.Component {
+    get users() {
+        return this.props.users.map(user => <User key={user.id} user={user} />);
+    }
+
+    render() {
+        return (
+            <div className='user'>
+                {this.users}
+            </div>
+        );
+    }
+}
+
+class User extends React.Component {
+    render() {
+        return (
+                <div className={'image'}>
+                    <img src={this.props.user.avatar_url} style={{ maxWidth: '100px' }} />            
+                    <p>
+                        <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+                    </p>
+                </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
